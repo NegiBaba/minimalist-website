66053c3a8d829db760254e43ea53c7d10ee8b727
NegiBaba <piyushnegi319@gmail.com>
2021-06-14 01:11:10 +0530

    Added Nodejs to the website to have backend functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const https = require("https");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+
+	https.get("https://zenquotes.io/api/random", function(response) {
+		response.on("data", function(data) {
+			const quote = JSON.parse(data);
+			//console.log(quote[0].q);
+			res.render("home", {quote: quote[0].q, author: quote[0].a});
+		})
+	});
+})
+app.listen(3000, function() {
+	console.log("Server running on port 3000");
+})
