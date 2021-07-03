406dfd606780412ff862e91944c43ed1a06816a3
NegiBaba <piyushnegi319@gmail.com>
2021-07-03 22:30:56 +0530

    Added fadeIn animation for the about section.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,11 @@
 $(window).on("load", function() {
 	$(".loader-container").fadeOut("fade");
 });
+
+$(document).ready(function() {
+	$(".js--about-section").waypoint(function(direction) {
+		$(".js--about-section").addClass('animate__animated animate__fadeIn');
+	}, {
+		offset: '70%'
+	});
+});
